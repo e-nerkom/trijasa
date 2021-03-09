7f266827f560ccb54f2fa74ba7dce0e9203374a3
muhammadzakinudin <zakinugraha001@gmail.com>
2021-03-09 11:04:26 +0700

    Create layouting admin

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,14 @@
+@section('sidebar')
+<div class="sidebar-heading">UI Components</div>
+<div class="sidebar-block p-0">
+    <ul class="sidebar-menu" id="components_menu">
+        <li class="sidebar-menu-item">
+            <a class="sidebar-menu-button" href="ui-buttons.html">
+                <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">mouse</i>
+                <span class="sidebar-menu-text">Buttons</span>
+            </a>
+        </li>
+        
+    </ul>
+</div>
+@endsection
